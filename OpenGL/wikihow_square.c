a26b8145597d97d759346dc0d0c959fa7d4e2f29
Bharat Mazire <mazire.bharat@gmail.com>
2022-11-22 11:24:34 +0530

    Create wikihow_square.c

---
+++ OpenGL/wikihow_square.c
@@ -0,0 +1,43 @@
+#include <GL/freeglut.h>
+#include <GL/gl.h>
+
+float x = 0.0f;
+
+void renderFunction()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+  
+    // set projection
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+  
+    // apply translation
+    glTranslatef(x,0.0f,0.0f);
+    // apply color
+    glColor3f(1.0, 1.0, 1.0);
+    
+    glBegin(GL_POLYGON);
+        glVertex2f(-0.5, -0.5);
+        glVertex2f(-0.5, 0.5);
+        glVertex2f(0.5, 0.5);
+        glVertex2f(0.5, -0.5);
+    glEnd();
+    glFlush();
+  
+  // update...
+  if(x < 5.0f)
+    x = x + 0.01f;
+}
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("OpenGL - First window demo");
+    glutDisplayFunc(renderFunction);
+    glutMainLoop();    
+    return 0;
+}
